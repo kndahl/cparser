382745df63bc7204630fac22a74a0e5b5e4a0575
Roman <REKarpukhin@sberbank.ru>
2021-08-28 19:05:56 +0300

    parser can find blocks now

--- include/xml.hpp
+++ include/xml.hpp
@@ -4,13 +4,19 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
+#include <array>
 
 class XML {
     private:
         std::string filename;
+        std::vector<std::string> blocks;  // TODO: Make it dynamic
+        std::string output;
         int         error;
     public:
-        int cparse_xml(std::string str);
+        int  cparse_xml(std::string str);
+        int  parse(std::string fl);
+        void get_blocks(std::string str);
 };
 
 #endif
